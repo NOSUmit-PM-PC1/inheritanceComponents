ec0fd524e556bb4b3e20e03391eee12ccac87f17
Makarenkomd <mahadrum@yandex.ru>
2022-05-15 19:30:29 +0300

    Добавьте файлы проекта.

---
+++ inheritanceComponents/Cards.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace inheritanceComponents
+{
+    class Card: PictureBox
+    {
+        int id;
+        public Card(int id, Control control)
+        {
+            this.id = id;
+            this.Load("cards/p" + id.ToString() + ".png");
+            this.Top = 10;
+            this.Left = 20;
+            control.Controls.Add(this);
+        }
+    }
+}
